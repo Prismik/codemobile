5bf52a7e0de5760ca4c744d639c37457e2cfd776
Francis <francis.beauchampsc@gmail.com>
2015-04-08 14:38:53 -0400

    Changes for demo

--- app.js
+++ app.js
@@ -13,4 +13,18 @@ $(document).ready(function() {
 	$('.js-anim').click(function () {
 		Epreuve.show();
 	});
+	$('.more').click(function() {
+		$('#more').prepend('<div class="ui-grid-b">' +
+			'<div class="ui-block-a">' +
+			'<div class="ui-bar ui-bar-a" style="height:60px">' +
+			'<a href="#algoPop" data-rel="popup" data-position-to="window" data-transition="pop">Recursion 2</a>' +
+			'</div></div><div class="ui-block-b">' +
+			'<div class="ui-bar ui-bar-a" style="height:60px">' +
+			'<a href="#algoPop" data-rel="popup" data-position-to="window" data-transition="pop">Bouger 3</a>' +
+			'</div></div><div class="ui-block-c">' +
+			'<div class="ui-bar ui-bar-a" style="height:60px">' +
+			'<a href="#algoPop" data-rel="popup" data-position-to="window" data-transition="pop">Boucle 3</a>' +
+			'</div></div></div>'
+		).trigger("create");
+	});
 });
